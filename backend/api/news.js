7ae2a9ca597abdaf37b97d6672de9dda040bb0df
Pivoz <43602900+Pivoz@users.noreply.github.com>
2020-03-30 10:54:50 +0200

    Commit before pulling

--- backend/api/news.js
+++ backend/api/news.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const errorHandler = require('../utils/errorHandler');
+const errorHandler = require('./utils/errorHandler');
 //const NewsImpl = requ
 
 const ModelValidationError = require('../model/exceptions/logic/modelValidationError');
